a031e32372702f79feb66598e6cf90cc99f98d5b
jimmy cruz <cruzperezjimmya@gmail.com>
2023-01-06 11:18:55 -0500

    envio del avance, aplicacion de directiva y alerta

--- src/app/directivas/validacion.directive.ts
+++ src/app/directivas/validacion.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 
 @Directive({
-  selector: '[appValidacion]'
+  selector: '[validar]' //directiva validar, permite evaluar si el input esta vacio
 })
-export class ValidacionDirective {
+export class ValidacionDirective implements OnInit{
 
-  constructor() { }
+  constructor( private me:ElementRef) {
+
+   }
+
+   @Input() color = "red";
+   @Input() nombre!:String;
+
+   ngOnInit():void {
+    if (this.nombre) {
+      
+    }
+    this.me.nativeElement.style.color = this.color;
+    this.me.nativeElement.style.borderColor = this.color;
+   }
+
+
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  color = 'red';
+  enviar(){
+    alert('GRACIAS POR COMUNICARNOS CON NOSOTROS')
+  }
 }
